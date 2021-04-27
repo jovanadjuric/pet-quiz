7d8930332a4fb124414a32116467234d831d1a36
jovanamatk <mat.jovana@yahoo.com>
2021-04-27 21:14:31 +0200

    feat: add two additional answers

--- src/services/questions.jsx
+++ src/services/questions.jsx
@@ -2,17 +2,17 @@ const questions = [
   {
     _id: 0,
     question: "Your perfect pet is:",
-    choices: ["small", "big"],
+    choices: ["small", "big", "cuddly", "lazy"],
   },
   {
     _id: 1,
     question: "Your perfect pet likes to:",
-    choices: ["eat a lot", "run a lot"],
+    choices: ["eat a lot", "run a lot", "sleep a lot", "jump around"],
   },
   {
     _id: 2,
     question: "Your perfect pet hates:",
-    choices: ["hot weather", "cold weather"],
+    choices: ["hot weather", "cold weather", "other animals", "water"],
   },
 ];
 
